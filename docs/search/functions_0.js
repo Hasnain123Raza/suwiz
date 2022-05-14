fd0f7afea6113f6e2c93c191ad8432833e4105c7
Hasnain123Raza <Hasnain123Raza@users.noreply.github.com>
2022-05-14 19:47:23 +0000

    deploy: deaad6f4f77179e0d5794374f05c0e70ac7b7b91

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['suwiz_5fboard_5fcreate_0',['suwiz_board_create',['../suwiz_8h.html#a57d09af93fdd150ef34f92ebea5409a5',1,'suwiz.h']]],
-  ['suwiz_5fboard_5ffree_1',['suwiz_board_free',['../suwiz_8h.html#a6f2db81b89cc31d449878f4267ad8058',1,'suwiz.h']]],
-  ['suwiz_5fboard_5fprint_2',['suwiz_board_print',['../suwiz_8h.html#aa4c2b36a238dc3ad2233476dd0616e49',1,'suwiz.h']]],
-  ['suwiz_5fboard_5fstatus_3',['suwiz_board_status',['../suwiz_8h.html#adc494f06a838f13579c2fc4c7e78ec4f',1,'suwiz.h']]]
+  ['suwiz_5fboard_5fcreate_0',['suwiz_board_create',['../suwiz_8h.html#aa2762f8a0420367743d33541ec15315b',1,'suwiz.h']]],
+  ['suwiz_5fboard_5ffree_1',['suwiz_board_free',['../suwiz_8h.html#a2475ec6444db0c1c10e35c4917374123',1,'suwiz.h']]],
+  ['suwiz_5fboard_5fprint_2',['suwiz_board_print',['../suwiz_8h.html#aab072d6647742e18d3f8730e5d8a753a',1,'suwiz.h']]],
+  ['suwiz_5fboard_5fstatus_3',['suwiz_board_status',['../suwiz_8h.html#a09368b730a6fb57fa0fe8d5d2774f7cb',1,'suwiz.h']]]
 ];
