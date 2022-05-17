7fb56ff4d6dcf98da9da675ddb655d3c423ea995
Hasnain Raza <hasnain.mohsan@gmail.com>
2022-05-17 16:30:23 -0400

    added solve utility function implementations

--- libsuwiz/src/solve.c
+++ libsuwiz/src/solve.c
@@ -1,11 +1,45 @@
 #include "solve.h"
 
 int16_t *suwiz_board_to_constraints(int8_t *board) {
-    return NULL;
+    if (board == NULL) {
+        return NULL;
+    }
+
+    enum SuwizBoardStatus status = suwiz_board_status(board);
+    if (status == SUWIZ_BOARD_STATUS_INVALID) {
+        return NULL;
+    }
+
+    int16_t *constraints = suwiz_constraints_create();
+    if (status == SUWIZ_BOARD_STATUS_SOLVED) {
+        for (int counter = 0; counter < SUWIZ_BOARD_SIZE; counter++) {
+            constraints[counter] = INT16_C(board[counter]);
+        }
+        return constraints;
+    }
+
+    for (int counter = 0; counter < SUWIZ_BOARD_SIZE; counter++) {
+        if (board[counter] != 0) {
+            suwiz_constraints_solve(constraints, counter, board[counter]);
+        }
+    }
+
+    return constraints;
 }
 
 int8_t *suwiz_constraints_to_board(int16_t *constraints) {
-    return NULL;
+    if (constraints == NULL) {
+        return NULL;
+    }
+
+    int8_t *board = suwiz_board_create(NULL);
+    for (int counter = 0; counter < SUWIZ_BOARD_SIZE; counter++) {
+        if (SUWIZ_CONSTRAINTS_IS_VALUE(constraints[counter])) {
+            board[counter] = INT8_C(constraints[counter]);
+        }
+    }
+
+    return board;
 }
 
 
