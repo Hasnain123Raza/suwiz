97ef8dc7033cc8108db880c328468c52db71011d
Hasnain123Raza <Hasnain123Raza@users.noreply.github.com>
2022-05-22 19:14:33 +0000

    deploy: f7b98b4a57fb4583cd07a14126fb65cd6885bb8e

--- docs/search/defines_0.js
+++ docs/search/defines_0.js
@@ -2,15 +2,16 @@ var searchData=
 [
   ['suwiz_5fboard_5fbox_5fsize_0',['SUWIZ_BOARD_BOX_SIZE',['../common_8h.html#a6840a294df6916f62045615f17f8ba4a',1,'common.h']]],
   ['suwiz_5fboard_5fboxes_1',['SUWIZ_BOARD_BOXES',['../common_8h.html#a89bacf6d305a74e10ec5be0497b5187e',1,'common.h']]],
-  ['suwiz_5fboard_5fcolumn_5fsize_2',['SUWIZ_BOARD_COLUMN_SIZE',['../common_8h.html#a88505d617bb7da9a945ad464c75bfe41',1,'common.h']]],
-  ['suwiz_5fboard_5fcolumns_3',['SUWIZ_BOARD_COLUMNS',['../common_8h.html#a64aa798de299aed3fbebe52dc5de87bc',1,'common.h']]],
-  ['suwiz_5fboard_5findex_5fbox_4',['SUWIZ_BOARD_INDEX_BOX',['../common_8h.html#a47fdea00b9e91996943afa3ca39c1a4d',1,'common.h']]],
-  ['suwiz_5fboard_5findex_5fcolumn_5',['SUWIZ_BOARD_INDEX_COLUMN',['../common_8h.html#a3e0f47a1b9f341886971b6e21a3f89e7',1,'common.h']]],
-  ['suwiz_5fboard_5findex_5frow_6',['SUWIZ_BOARD_INDEX_ROW',['../common_8h.html#aabe701bc3f50ee0d0a440ce2c0223c58',1,'common.h']]],
-  ['suwiz_5fboard_5findex_5fto_5fbox_7',['SUWIZ_BOARD_INDEX_TO_BOX',['../common_8h.html#aa8f06c10dea9ad3d73681692f6d677c6',1,'common.h']]],
-  ['suwiz_5fboard_5findex_5fto_5fcolumn_8',['SUWIZ_BOARD_INDEX_TO_COLUMN',['../common_8h.html#a5c491870ba8ec0e86f040de365d381a5',1,'common.h']]],
-  ['suwiz_5fboard_5findex_5fto_5frow_9',['SUWIZ_BOARD_INDEX_TO_ROW',['../common_8h.html#a500f99e17bef6d0b3e796c5e02454715',1,'common.h']]],
-  ['suwiz_5fboard_5frow_5fsize_10',['SUWIZ_BOARD_ROW_SIZE',['../common_8h.html#a28d766248bd257e453731165566e9aa7',1,'common.h']]],
-  ['suwiz_5fboard_5frows_11',['SUWIZ_BOARD_ROWS',['../common_8h.html#a464814832880b3dce54340e2f2c30533',1,'common.h']]],
-  ['suwiz_5fboard_5fsize_12',['SUWIZ_BOARD_SIZE',['../common_8h.html#a68fa26d66f9fa1810c3f3e53041bf3ff',1,'common.h']]]
+  ['suwiz_5fboard_5fchoices_2',['SUWIZ_BOARD_CHOICES',['../common_8h.html#aabed703686be1d739b33443b791b7b51',1,'common.h']]],
+  ['suwiz_5fboard_5fcolumn_5fsize_3',['SUWIZ_BOARD_COLUMN_SIZE',['../common_8h.html#a88505d617bb7da9a945ad464c75bfe41',1,'common.h']]],
+  ['suwiz_5fboard_5fcolumns_4',['SUWIZ_BOARD_COLUMNS',['../common_8h.html#a64aa798de299aed3fbebe52dc5de87bc',1,'common.h']]],
+  ['suwiz_5fboard_5findex_5fbox_5',['SUWIZ_BOARD_INDEX_BOX',['../common_8h.html#a47fdea00b9e91996943afa3ca39c1a4d',1,'common.h']]],
+  ['suwiz_5fboard_5findex_5fcolumn_6',['SUWIZ_BOARD_INDEX_COLUMN',['../common_8h.html#a3e0f47a1b9f341886971b6e21a3f89e7',1,'common.h']]],
+  ['suwiz_5fboard_5findex_5frow_7',['SUWIZ_BOARD_INDEX_ROW',['../common_8h.html#aabe701bc3f50ee0d0a440ce2c0223c58',1,'common.h']]],
+  ['suwiz_5fboard_5findex_5fto_5fbox_8',['SUWIZ_BOARD_INDEX_TO_BOX',['../common_8h.html#aa8f06c10dea9ad3d73681692f6d677c6',1,'common.h']]],
+  ['suwiz_5fboard_5findex_5fto_5fcolumn_9',['SUWIZ_BOARD_INDEX_TO_COLUMN',['../common_8h.html#a5c491870ba8ec0e86f040de365d381a5',1,'common.h']]],
+  ['suwiz_5fboard_5findex_5fto_5frow_10',['SUWIZ_BOARD_INDEX_TO_ROW',['../common_8h.html#a500f99e17bef6d0b3e796c5e02454715',1,'common.h']]],
+  ['suwiz_5fboard_5frow_5fsize_11',['SUWIZ_BOARD_ROW_SIZE',['../common_8h.html#a28d766248bd257e453731165566e9aa7',1,'common.h']]],
+  ['suwiz_5fboard_5frows_12',['SUWIZ_BOARD_ROWS',['../common_8h.html#a464814832880b3dce54340e2f2c30533',1,'common.h']]],
+  ['suwiz_5fboard_5fsize_13',['SUWIZ_BOARD_SIZE',['../common_8h.html#a68fa26d66f9fa1810c3f3e53041bf3ff',1,'common.h']]]
 ];
